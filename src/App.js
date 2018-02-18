ef0d4516941d9c8a59f12b18fb1356f51ad8ab74
Suthon <suthon@coe.psu.ac.th>
2018-02-18 09:45:08 +0700

    simple nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import HomePage from './components/HomePage';
+import NamePage from './components/NamePage';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={HomePage}/>
+        <div>
+          <div>
+            <Link to="/">Home</Link> | <Link to="/name">Name</Link>
+          </div>
+          <div>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/name" component={NamePage}/>
+          </div>
+        </div>
       </Router>
     );
   }
