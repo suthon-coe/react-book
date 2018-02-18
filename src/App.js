e770f717f8386efc8bd605961efca6d677b8df62
Suthon <suthon@coe.psu.ac.th>
2018-02-18 09:55:00 +0700

    book api (fake)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import HomePage from './components/HomePage';
 import NamePage from './components/NamePage';
 
@@ -11,10 +11,10 @@ class App extends Component {
           <div>
             <Link to="/">Home</Link> | <Link to="/name">Name</Link>
           </div>
-          <div>
+          <Switch>
             <Route exact path="/" component={HomePage}/>
             <Route path="/name" component={NamePage}/>
-          </div>
+          </Switch>
         </div>
       </Router>
     );
---
+++ src/api.js
@@ -0,0 +1,18 @@
+const BookAPI = {
+    getNames: function(){
+        return [
+            {
+            "list_name": "Combined Print and E-Book Fiction",
+            "display_name": "Combined Print & E-Book Fiction",
+            "list_name_encoded": "combined-print-and-e-book-fiction",
+            },
+            {
+            "list_name": "Combined Print and E-Book Nonfiction",
+            "display_name": "Combined Print & E-Book Nonfiction",
+            "list_name_encoded": "combined-print-and-e-book-nonfiction",
+            }
+        ]
+    }
+}
+
+export default BookAPI;
