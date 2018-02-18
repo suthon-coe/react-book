b97c9f38dbddc64e9568b4b96b37e435d7ae3bbf
Suthon <suthon@coe.psu.ac.th>
2018-02-18 09:38:24 +0700

    simple route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello world
-      </div>
+      <Router>
+        <Route exact path="/" component={HomePage}/>
+      </Router>
     );
   }
 }
---
+++ src/components/NamePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NamePage extends Component {
+  render() {
+    return (
+      <div>
+        Name
+      </div>
+    );
+  }
+}
+
+export default NamePage;
