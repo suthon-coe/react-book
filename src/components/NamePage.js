49266bdc47dd67d70ceeb6069bf794bc411a99c8
Suthon <suthon@coe.psu.ac.th>
2018-02-18 10:02:23 +0700

    name list

--- src/components/NamePage.js
+++ src/components/NamePage.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import BookAPI from '../api';
+const NameItem = ({list_name}) => (
+    <div>{list_name}</div>
+)
 
 class NamePage extends Component {
-  render() {
-    return (
-      <div>
-        Name
-      </div>
-    );
-  }
+    constructor(props){
+        super(props)
+        this.state = {names: []}
+    }
+    componentDidMount(){
+        let names = BookAPI.getNames()
+        this.setState({names})
+    }
+    render() {
+        return (
+        <div>
+            {
+                this.state.names.map(n => <NameItem {...n}/>)
+            }
+        </div>
+        );
+    }
 }
 
 export default NamePage;
